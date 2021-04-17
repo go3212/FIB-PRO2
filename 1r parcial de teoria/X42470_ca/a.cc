5db772213fc4eb364a18363256166816060ef46c
Fernando <go3212@hotmail.com>
2021-04-17 20:10:50 +0200

    Inicializado

---
+++ 1r parcial de teoria/X42470_ca/a.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "BinTree.hh"
+
+using namespace std;
+
+/* Pre: a=A */
+/* Post: agd es un arbre amb la mateixa estructura que A on cada
+   node conte el grau de desequilibri del subarbre d'A corresponent */
+void arbre_graus_desequilibri(const BinTree<int> &a, BinTree<int> &agd)
+{
+    
+}
---
+++ 1r parcial de teoria/X77648_ca/a.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "BinTree.hh"
+
+using namespace std;
+
+/* Pre: cert */
+/* Post: el resultat indica si a i b son quasi coincidents */
+bool quasi_coincidents (const BinTree<int> &a, const BinTree<int> &b)
+{
+    
+}
