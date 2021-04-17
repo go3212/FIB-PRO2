9b5e7702465a0ae3cff7b6cd6861917ad94312ab
Fernando <go3212@hotmail.com>
2021-04-17 20:04:39 +0200

    Preferential path in a tree (BinTree)

---
+++ 1r parcial de teoria/X78332_en/a.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "BinTree.hh"
+#include <stack>
+
+using namespace std;
+
+/* Pre: c is empty */
+/* Post: c contains the preferential path of a; if it is non-empty, the
+         first element of the path is on the top of c */
+void cami_preferent(const BinTree<int>& a, stack<int>& c)
+{
+    
+}
